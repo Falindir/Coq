63d33f92ace4fb7253fad95ca9d80cefe4bc4b46
Lopez Jimmy <jimmy.lopez@etud.univ-montp2.fr>
2015-10-09 14:21:06 +0200

    Lemme 1

--- tp3.v
+++ tp3.v
@@ -8,3 +8,29 @@ simpl.
 rewrite H.
 reflexivity.
 
+Fixpoint f (n : nat) : nat :=
+match n with
+| 0 => 1
+| S p => (2 * (f p)) 
+end.
+
+Goal f(10) = 1024.
+simpl.
+reflexivity.
+
+Open Scope list.
+Require Import List.
+Import ListNotations.
+
+Lemma P1 : forall E : Type, forall l : (list E) , forall e : E, rev(l ++ [e]) = e :: rev l. 
+intros.
+elim l.
+simpl.
+reflexivity.
+intros.
+simpl.
+rewrite H.
+simpl.
+reflexivity.
+
+
